d5e57157d3a0d17c494416086e64bd7e964f4141
Arman Hossen <2armanhossen@gmail.com>
2025-12-02 15:11:33 +0600

    js
    again brooooooooooooooo

---
+++ js web1 - Intro/index.js
@@ -0,0 +1,23 @@
+// to output a text
+console.log("Arman Hossen Ripon");
+// inside () of console.log, i can use "", '', `` — those are called “backticks.”
+let name1 = `Khan`;
+console.log(name1);
+// what ever we write to print the liveserver will show in the inspect and console page
+
+// create a alert message
+// window.alert(`this is an alert!`);
+// window.alert(`relax bro i am not hak3r`);
+
+// this is a comment
+
+/*
+this 
+is 
+a 
+multiline 
+comment
+*/
+
+document.getElementById("nameOfAuthor").textContent =  `Hello World`;
+document.getElementById("wp").textContent = `Well, this is a textContent`;
